4818b4751df4ed8e1a54adb679f8957a393cb530
Ensawr <ensarhayiroglu@gmail.com>
2024-07-14 05:26:37 +0300

    navbar added

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-sm text-center py-16'>2024 © Made by <a className='text-yellow-400 underline' href="https://github.com/Ensawr">ensawr</a></div>
+    <div className='text-sm text-center pt-16 pb-12'>2024 © Made by <a className='text-yellow-400 underline' href="https://github.com/Ensawr">ensawr</a></div>
   )
 }
 
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Logo from '../images/pokemon-logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-full bg-white border-b-[1px] shadow-lg mb-4'>
+      <div className='max-w-[1440px] mx-auto'>
+        <div>
+          <a href="/"><img className='max-w-40 mx-auto' src={Logo} alt="" /></a>
+        </div>
+      </div>
+    </div>
   )
 }
 
